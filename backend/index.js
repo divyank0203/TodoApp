13f9aceb05d2daec70aca005d5fd175122a77f38
divyank0203 <divyankh444@gmail.com>
2025-09-01 04:03:05 +0530

    added zod validation in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createTodoschema } = require("./types");
 const app = express();
 // const zod = require("zod");
 
@@ -10,7 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.post('/todo', function(req, res){ //to create a todo
-
+const newTodo = req.body;
+const val = createTodoschema.safeParse(newTodo);
 })
 
 app.get('/todos', function(req, res){ //to get all todos or a specific todo
@@ -18,7 +20,8 @@ app.get('/todos', function(req, res){ //to get all todos or a specific todo
 })
 
 app.put('/completed', function(req, res){ // to mark a todo as completed
-
+const todoId = req.body.id;
+const val2 = updateTodoschema.safeParse(todoId);
 })
 
 
