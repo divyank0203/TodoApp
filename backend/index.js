c88f6ba224961991d68dd4825dc335ef4c7a7a08
divyank0203 <divyankh444@gmail.com>
2025-09-02 22:40:11 +0530

    backend boilerplate

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createTodoschema } = require("./types");
+const { createTodoschema, updateTodoschema } = require("./types");
 const app = express();
 // const zod = require("zod");
 
@@ -13,6 +13,13 @@ app.use(express.json());
 app.post('/todo', function(req, res){ //to create a todo
 const newTodo = req.body;
 const val = createTodoschema.safeParse(newTodo);
+if(!val.success){
+    res.status(411).json({
+        msg: "You sent the wrong inputs"
+    })
+    return;
+}
+//if the input format is correct, put this in mongodb and create a todo
 })
 
 app.get('/todos', function(req, res){ //to get all todos or a specific todo
@@ -20,8 +27,14 @@ app.get('/todos', function(req, res){ //to get all todos or a specific todo
 })
 
 app.put('/completed', function(req, res){ // to mark a todo as completed
-const todoId = req.body.id;
+const todoId = req.body;
 const val2 = updateTodoschema.safeParse(todoId);
+if(!val2.success){
+    res.status(411).json({
+        msg: "You sent the wrong inputs"
+    })
+    return;
+}
 })
 
 
