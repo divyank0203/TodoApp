e79ec03c3316d136424ed10578c3dfab03c7752a
divyank0203 <divyankh444@gmail.com>
2025-09-03 21:14:55 +0530

    todos logic + cors backend access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CreateTodo from './Components/CreateTodo'
+import Todos from './Components/Todos'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  fetch("http://localhost:3000/todos")
+    .then(async function(response){ 
+      const json = await response.json();
+      setTodos(json);
+    });
+
+
 
   return (
     <>
       <CreateTodo />
+      <Todos todos={todos} ></Todos>
+    
     </>
   )
 }
--- frontend/src/Components/Todos.jsx
+++ frontend/src/Components/Todos.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
-function Todos() {
+//todos = {
+//  id: 1,
+//  title: "Sample Todo",
+//  description: "This is a sample todo item",
+//  completed: false
+//}
+
+function Todos({todos}) {
   return (
-    <div>Todos</div>
+    <div>
+        {/* <div>
+            <h1 className='text-xl font-bold'>{todos[0].title}</h1>
+            <h2 className='text-lg'>{todos[0].description}</h2>
+            <button className={`p-2 rounded ${todos[0].completed ? "bg-green-500" : "bg-red-500"}`}>
+              {todos[0].completed === true ? "Completed" : "Not Completed"}
+            </button>
+        </div> */}
+        {todos.map(function(todo){
+            return <div className='border p-4 m-2 rounded' key={todo.id}>
+                <h1 className='text-xl font-bold m-2'>{todo.title}</h1>
+                <h2 className='text-lg m-2'>{todo.description}</h2>
+                <button className={`p-2 m-2 rounded ${todo.completed ? "bg-green-500" : "bg-red-500"}`}>{todo.completed === true ? "Completed" : "Mark As Complete"}</button>
+            </div>
+        })}
+    </div>
   )
 }
 
